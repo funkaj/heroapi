e245a230bc338af0f7d600b313d6bdbb2433d121
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2019-02-08 13:51:01 -0800

    changed grid layout for search result

--- client/src/components/ResultList/ResultList.js
+++ client/src/components/ResultList/ResultList.js
@@ -1,57 +1,62 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import './ResultList.css';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import StatList from '../StatList/StatList';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
+// import tileData from './tileData';
+import './ResultList.css';
 
-const styles = {
-	card: {
-		minWidth: 275,
+const styles = theme => ({
+	root: {
+		display: 'flex',
+		flexWrap: 'wrap',
+		justifyContent: 'space-around',
+		overflow: 'hidden',
+		backgroundColor: theme.palette.background.paper,
 	},
-	bullet: {
-		display: 'inline-block',
-		margin: '0 2px',
-		transform: 'scale(0.8)',
-	},
-	title: {
-		fontSize: 14,
-	},
-	pos: {
-		marginBottom: 12,
-	},
-};
+});
+/**
+ * The example data is structured as follows:
+ *
+ * import image from 'path/to/image.jpg';
+ * [etc...]
+ *
+ * const tileData = [
+ *   {
+ *     img: image,
+ *     title: 'Image',
+ *     author: 'author',
+ *     cols: 2,
+ *   },
+ *   {
+ *     [etc...]
+ *   },
+ * ];
+ */
 function ResultList(props) {
 	const { classes } = props;
 
 	return (
-		<div>
-			<ul className='list-group' style={{ listStyleType: 'none' }}>
-				<Grid container>
-					{props.results.map(result => (
-						<Grid item xs={12} md={6}>
-							<Card className={classes.card}>
-								<CardContent>
-									<li className='list-group-item hero' key={result.id}>
-										<img
-											key={result.image.small_url}
-											alt={result.name}
-											className='img-fluid'
-											src={result.image.small_url}
-										/>
-										<p key={result.publisher.name}>{result.publisher.name}</p>
-										<h1 key={result.name}>{result.name}</h1>
-										<p key={result.slug}>{result.deck}</p>
-										<StatList res={result} />
-									</li>
-								</CardContent>
-							</Card>
-						</Grid>
-					))}
-				</Grid>
-			</ul>
+		<div className={classes.root}>
+			<GridList cellHeight={500} className={classes.gridList} cols={3}>
+				{props.results.map(result => (
+					<GridListTile key={result.image.small_url} cols={result.cols || 1}>
+						<img src={result.image.small_url} alt={result.id} />
+						<GridListTileBar
+							title={result.name}
+							subtitle={<span>by: {result.name}</span>}
+							actionIcon={
+								<IconButton className={classes.icon}>
+									<InfoIcon />
+								</IconButton>
+							}
+						/>
+					</GridListTile>
+				))}
+			</GridList>
 		</div>
 	);
 }
